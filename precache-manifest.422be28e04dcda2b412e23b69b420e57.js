2e3640eff5ee21e8e9407061859710eabf8607c8
Patrice Diouf <trispa88@gmail.com>
2020-04-04 20:12:09 -0400

    Updates

---
+++ precache-manifest.422be28e04dcda2b412e23b69b420e57.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ef54b7f48150301e3f03f3051fc1274",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5a003d6b008fbd1cb76",
+    "url": "/static/css/2.bce3ab99.chunk.css"
+  },
+  {
+    "revision": "65df802e103f0d0af8ba",
+    "url": "/static/css/main.11e39e45.chunk.css"
+  },
+  {
+    "revision": "d5a003d6b008fbd1cb76",
+    "url": "/static/js/2.844e5f3a.chunk.js"
+  },
+  {
+    "revision": "ab4be8b5be2c7bd01d3e3ef68fc32c54",
+    "url": "/static/js/2.844e5f3a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "65df802e103f0d0af8ba",
+    "url": "/static/js/main.b0a07ef1.chunk.js"
+  },
+  {
+    "revision": "1ec456e3b8f1603056ff",
+    "url": "/static/js/runtime-main.17c5ceca.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
